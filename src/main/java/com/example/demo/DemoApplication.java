d27637bb0bad74e0630a1cf97f12bd22ba380b4f
Sudhakar <125473501+sudhakarganesh@users.noreply.github.com>
2024-02-26 00:27:31 -0500

    Update DemoApplication.java
    Post message added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +14,11 @@ public class DemoApplication {
 	@GetMapping("/message")
 	public String displayMessage() {
 		return "Hi Message from Ganesh Sudhakar !";
+	}
+	
+	@PostMapping("/{name}")
+	public String findByName(@RequestBody String name) {
+		return "#### Sudhakar ganesh printed name by parameter : "+ name;
 	}
 
 	public static void main(String[] args) {
